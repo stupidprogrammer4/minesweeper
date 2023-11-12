f8fe9f82766194f4a36f12c014100c02f23fb2a9
stupidprogrammer764 <ilovelinux764@gmail.com>
2023-11-13 02:26:03 +0330

    most usages header files

---
+++ src/common.h
@@ -0,0 +1,28 @@
+#ifndef __COMMON__
+#define __COMMON__
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_thread.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef size_t usize;
+
+typedef float f32;
+typedef double f64;
+
+#endif // __COMMON__
