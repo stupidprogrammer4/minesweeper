3dc566fec56d2b8d917b0851bd25d7456a6140f1
Joker644644 <captainjoker757@gmail.com>
2023-11-17 03:18:34 +0330

    add build

---
+++ build/CMakeFiles/minesweeper.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joker/programming/mabani_projects/minesweeper/src/dictionary.c" "CMakeFiles/minesweeper.dir/src/dictionary.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/dictionary.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/game.c" "CMakeFiles/minesweeper.dir/src/game.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/game.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/list.c" "CMakeFiles/minesweeper.dir/src/list.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/list.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/main.c" "CMakeFiles/minesweeper.dir/src/main.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/main.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/manager.c" "CMakeFiles/minesweeper.dir/src/manager.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/manager.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/ui.c" "CMakeFiles/minesweeper.dir/src/ui.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/ui.c.o.d"
+  "/home/joker/programming/mabani_projects/minesweeper/src/utils.c" "CMakeFiles/minesweeper.dir/src/utils.c.o" "gcc" "CMakeFiles/minesweeper.dir/src/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
